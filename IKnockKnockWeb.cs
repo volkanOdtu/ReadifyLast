cec77a8ab568dd0551e487b736b596d1630dac8f
volkan <volkan510@gmail.com>
2018-11-30 02:53:42 +1100

    First commit

---
+++ IKnockKnockWeb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+
+namespace WcfServiceApp
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IKnockKnockWeb" in both code and config file together.
+    [ServiceContract]
+    public interface IKnockKnockWeb
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "api/Fibonacci?n={n}")]
+        int Fibonacci(int n);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "api/ReverseWords?sentence={sentence}")]
+        string reverse(String sentence);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "api/Token")]
+        string getToken();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "api/TriangleType?a={a}&b={b}&c={c}")]
+        string TriangleType(int a, int b, int c);
+
+    }
+}
